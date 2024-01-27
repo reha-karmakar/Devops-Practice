7cf1369d3d06ddd9e3649adab4524577c9c2f241
reha-karmakar <rehakarmakar14@gmail.com>
2024-01-27 13:12:49 +0530

    Addition Function

---
+++ calculator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#########################
+#Author: Reha Karmakar
+#Date: 27/01/2024
+#
+#This script outputs basic calculator functions
+#
+#Version:v1
+########################
+
+a=4
+b=5
+
+#Addition Function
+echo $(a+b)
+
+
